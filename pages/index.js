8d190bea492bea66232c2dc39383906274e4481e
ansxy <ansarfadillah17@gmail.com>
2023-03-13 05:37:57 +0800

    .

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 import NavBar from './components/navbar'
 import Hero from './components/hero'
 import Profile from './components/profile'
