d5167558f672d98c02de9d2dc2bd7b974b64eaf5
ansxy <ansarfadillah17@gmail.com>
2023-09-09 16:34:51 -0400

    refac : change color on footer contact

--- pages/components/contact.js
+++ pages/components/contact.js
@@ -40,7 +40,7 @@ export default function Contact() {
     }
   };
   return (
-    <div id="three" className="h-[20rem] flex">
+    <div className="h-[20rem] flex bg-[#c2fcf3]">
       <section className="flex w-full flex-col items-center">
         <div className="w-full flex">
           <div className="border-t-2 border-black w-full"></div>
@@ -50,59 +50,7 @@ export default function Contact() {
           <div className="border-t-2 border-black w-full"></div>
         </div>
         <section className="w-full flex flex-row justify-center">
-          {/* <form
-            className="w-full mt-10 flex-col flex items-center gap-5 text-black"
-            method="post"
-            onSubmit={handleSubmit}
-          >
-            <h1 className="text-black text-xl font-medium">
-              Drop me a line if you have some interesting to discuss 😸😸😸
-            </h1>
-            <div className="flex flex-col w-[20rem]">
-              <label for="email">Your Email</label>
-              <input
-                className="h-12 p-2 bg-white"
-                name="email"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="flex flex-col w-[20rem]">
-              <label for="name">Your Name</label>
-              <input
-                className="h-12 p-2 bg-white"
-                name="name"
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div className="flex flex-col w-[20rem]">
-              <label for="subject">Subject</label>
-              <input
-                className="h-12 p-2 bg-white"
-                name="subject"
-                type="text"
-                value={subject}
-                onChange={(e) => setSubject(e.target.value)}
-              />
-            </div>
-            <div className="flex flex-col w-[20rem]">
-              <label for="message">Message</label>
-              <textarea
-                className=" max-h-40 bg-white p-2"
-                name="message"
-                type="text"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-              />
-            </div>
-            <button className="btn btn-primary" type="submit" disabled = {statusMessage}>
-              Submit
-            </button>
-          </form> */}
-          <div className="mt-10">
+          <div className="mt-10 " id="three" >
             <div className="w-full items-center flex justify-center flex-col">
               <p className="text-bold text-xl text-black font-medium">
                 Reach Me At
@@ -112,7 +60,6 @@ export default function Contact() {
             </div>
           </div>
         </section>
-        <p className="mt-5 text-black">Handracted by me&#169;2022</p>
       </section>
     </div>
   );
