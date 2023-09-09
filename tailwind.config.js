d51d4db94a857105ea05e2346ffb480d408b1475
ansxy <ansarfadillah17@gmail.com>
2023-09-09 16:33:04 -0400

    add fira code font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        'fireCode':['Fira Code', 'monospace']
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
