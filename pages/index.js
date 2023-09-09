4f93f6a11b2653bb811cef209e891b96f7bc16b6
ansxy <ansarfadillah17@gmail.com>
2023-09-09 16:33:30 -0400

    change font on wesbite

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavBar/>
-      <main>
+      <main className='flex flex-col font-fireCodeode'>
         <Hero/>
         <motion.div
           initial={{ opacity: 0 }}
